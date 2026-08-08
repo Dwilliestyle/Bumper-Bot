346831dfe8d3b4c74967ed7568260950ee838f32
Don <dwilliestyle@gmail.com>
2026-08-08 12:34:38 -0400

    Update robot_control.ino

--- bumperbot_firmware/firmware/robot_control/robot_control.ino
+++ bumperbot_firmware/firmware/robot_control/robot_control.ino
@@ -9,9 +9,9 @@
 #define L298N_in1 12  // Dir Motor A
 
 // Wheel Encoders Connection PINs
-#define right_encoder_phaseA 3  // Interrupt 
+#define right_encoder_phaseA 2  // Interrupt 
 #define right_encoder_phaseB 5  
-#define left_encoder_phaseA 2   // Interrupt
+#define left_encoder_phaseA 3   // Interrupt
 #define left_encoder_phaseB 4
 
 // Encoders
